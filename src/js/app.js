1f6d9ebb742426b0920b60031f6d372a377db261
Shaquille Haynes <nothintodoee@yahoo.com>
2018-06-01 20:25:14 -0400

    adding mini logo scroll behavior

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,24 @@
-console.log("this is a test");
+var section_a = document.querySelector("#section-a");
+var section_b = document.querySelector("#section-b");
+var section_c = document.querySelector("#section-c");
+var skills = document.querySelector("#skills");
+var projects = document.querySelector("#projects");
+var topScroll = document.querySelector("#top-scroll");
 
-console.log("minify me please");
+window.addEventListener('load', function(){
+	skills.addEventListener('click', function(e){
+		e.preventDefault();
+		section_b.scrollIntoView({behavior: 'smooth'});
+	});
 
-console.log("hello")
+	projects.addEventListener('click', function(e){
+		e.preventDefault();
+		section_c.scrollIntoView({behavior: 'smooth'});
+	});
+
+	topScroll.addEventListener('click', function(e){
+		e.preventDefault();
+		section_a.scrollIntoView({behavior: 'smooth'})
+	});
+
+})
