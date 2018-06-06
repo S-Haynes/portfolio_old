5048c6302b7444186ad23f3d0008195cae893518
Shaquille Haynes <nothintodoee@yahoo.com>
2018-06-06 01:38:02 -0400

    adding nav mobile hamburger & responsiveness

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,38 @@
 var section_a = document.querySelector("#section-a");
 var section_b = document.querySelector("#section-b");
 var section_c = document.querySelector("#section-c");
-var skills = document.querySelector("#skills");
-var projects = document.querySelector("#projects");
+var skills = document.querySelectorAll(".skills");
+var projects = document.querySelectorAll(".projects");
+var contact = document.querySelectorAll(".contact");
+var dropdown = document.querySelector("#dropdown");
+var dropdown_info = document.querySelector("#dropdown-info");
+var dropdown_close = document.querySelector("#dropdown-close");
+var dropdown_underlay = document.querySelector("#dropdown-info-underlay");
 var topScroll = document.querySelector("#top-scroll");
 var about_me = document.querySelector("#about-me");
 var about_container = document.querySelector("#about-container");
 var closeBtn = document.querySelector("#close-button");
 var about_text = document.querySelector("#about-text");
 var contact_text = document.querySelector("#contact-text");
-var contact = document.querySelector("#contact");
+
 
 window.addEventListener('load', function(){
-	skills.addEventListener('click', function(e){
+	for(var i = 0; i < skills.length; i++){
+	skills[i].addEventListener('click', function(e){
 		e.preventDefault();
 		section_b.scrollIntoView({behavior: 'smooth'});
 	});
-
-	projects.addEventListener('click', function(e){
+	projects[i].addEventListener('click', function(e){
 		e.preventDefault();
 		section_c.scrollIntoView({behavior: 'smooth'});
 	});
 
+	contact[0].addEventListener('click', function(){
+		displayAbout();
+	})
+
+	}
+		
 	topScroll.addEventListener('click', function(e){
 		e.preventDefault();
 		section_a.scrollIntoView({behavior: 'smooth'})
@@ -35,9 +46,46 @@ window.addEventListener('load', function(){
 		hideAbout();
 	});
 
-	contact.addEventListener('click', function(){
-		displayAbout();
+	dropdown.addEventListener('click', function(){
+		dropdown_info.classList.remove('hidden');
+		dropdown_underlay.classList.remove('hidden');
+		setTimeout(function(){
+			dropdown_underlay.style.transform = "translateX(0%)"
+		}, 200)
+		
+		setTimeout(function(){
+		dropdown_info.style.transform = "translateX(0%)";
+		}, 400)
+		
 	})
+	dropdown_close.addEventListener('click', function(){
+		
+		dropdown_info.style.transform = "translateX(-100%)";
+		setTimeout(function(){
+			dropdown_underlay.style.transform = "translateX(-100%)";
+		}, 200)
+		
+		setTimeout(function(){
+			dropdown_info.classList.add('hidden');
+			dropdown_underlay.classList.add('hidden');
+		}, 800)
+
+	})
+	contact[1].addEventListener('click', function(){
+	dropdown_info.style.transform = "translateX(-100%)";
+		setTimeout(function(){
+			dropdown_underlay.style.transform = "translateX(-100%)";
+		}, 200)
+		
+		setTimeout(function(){
+			dropdown_info.classList.add('hidden');
+			dropdown_underlay.classList.add('hidden');
+		}, 800)
+		setTimeout(function(){
+			displayAbout();
+		}, 500)
+	})
+	
 
 	function displayAbout(){
 		about_container.classList.remove('hidden');
